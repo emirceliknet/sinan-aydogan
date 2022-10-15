5edd47c10cd475efad9ba016ddb16584c570e7ed
Sinan AYDOĞAN <sinanaydogan@hotmail.com.tr>
2022-10-15 16:25:32 +0300

    initial commit

---
+++ resources/js/Pages/Customer/IndexPage.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {Inertia} from "@inertiajs/inertia";
+
+defineProps({
+  customers: Array
+})
+
+const deleteItem = (id)=>{
+  Inertia.delete(route('customer.destroy', id));
+}
+</script>
+
+<template>
+  Müşteri listesi <br>
+  <ul>
+
+    <template v-for="customer in customers">
+      <li>
+        <div class="flex space-x-6">
+          <span>{{customer.name}}</span>
+          <span class="text-rose-500" @click="deleteItem(customer.id)">Sil</span>
+        </div>
+      </li>
+    </template>
+  </ul>
+</template>
